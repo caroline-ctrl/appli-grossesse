e13e679d1e4e2864d266639283cd32e2e48fda00
caroline-ctrl <duon.caroline@gmail.com>
2021-01-14 22:24:57 +0100

    creation de la table

--- database/migrations/2021_01_14_211309_create_users_table.php
+++ database/migrations/2021_01_14_211309_create_users_table.php
@@ -15,6 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->string("lastname");
+            $table->date("birthday");
+            $table->string("email");
+            $table->string("password");
             $table->timestamps();
         });
     }
